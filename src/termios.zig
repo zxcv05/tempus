bef732c8615361262ed4389e6cd848d0879dcf2d
zxcv05 <zxcv05@tuta.com>
2024-11-12 00:11:24 -0500

    Use std.posix wherever possible

--- src/termios.zig
+++ src/termios.zig
@@ -17,18 +17,11 @@ const stdin = c.STDIN_FILENO;
 
 const Termios = @This();
 
-original: c_termios.termios,
+original: std.posix.termios,
 
-// TODO(correctness): use std.posix functions where possible
 pub fn init() !Termios {
-    var original: c_termios.termios = undefined;
-
-    try wrap_posix_return(
-        c_termios.tcgetattr(stdout, &original),
-    );
-
     return Termios{
-        .original = original,
+        .original = try std.posix.tcgetattr(stdout),
     };
 }
 
@@ -36,29 +29,21 @@ pub fn init() !Termios {
 /// Set non-blocking on stdin
 pub fn set(this: *const Termios) !void {
     var copy = this.original;
-    c_termios.cfmakeraw(&copy);
+    c_termios.cfmakeraw(@ptrCast(&copy));
 
-    try wrap_posix_return(
-        c_termios.tcsetattr(stdout, c_termios.TCSADRAIN, &copy),
-    );
+    try std.posix.tcsetattr(stdout, .DRAIN, copy);
 
-    const flags = c.fcntl(stdin, c.F_GETFL);
-    try wrap_posix_return(
-        c.fcntl(stdin, c.F_SETFL, flags | c.O_NONBLOCK),
-    );
+    const flags = try std.posix.fcntl(stdin, std.posix.F.GETFL, 0);
+    _ = try std.posix.fcntl(stdin, std.posix.F.SETFL, flags | c.O_NONBLOCK);
 }
 
 /// Reset stdout to state prior to raw mode
 /// Unset non-blocking on stdin
 pub fn reset(this: *const Termios) !void {
-    const flags = c.fcntl(stdin, c.F_GETFL);
-    try wrap_posix_return(
-        c.fcntl(stdin, c.F_SETFL, flags & ~c.O_NONBLOCK),
-    );
+    const flags = try std.posix.fcntl(stdin, std.posix.F.GETFL, 0);
+    _ = try std.posix.fcntl(stdin, std.posix.F.SETFL, flags & ~@as(usize, c.O_NONBLOCK));
 
-    try wrap_posix_return(
-        c_termios.tcsetattr(stdout, c_termios.TCSAFLUSH, &this.original),
-    );
+    try std.posix.tcsetattr(stdout, .FLUSH, this.original);
 }
 
 /// Query ioctl for window size
